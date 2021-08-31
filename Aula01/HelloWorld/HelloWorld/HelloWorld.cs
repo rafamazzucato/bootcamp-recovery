27525dea85359a4637ddd28d71693ad4e2c9b875
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2021-08-31 17:42:23 -0300

    meu primeiro codigo C#

---
+++ Aula01/HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] argumentos)
+        {
+            Console.WriteLine("Meu primeiro codigo C# manual");
+        }
+    }
+}
