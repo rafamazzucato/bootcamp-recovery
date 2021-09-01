f1c1ff757af8bb380dbb228a4de3a5f44f242d6c
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2021-09-01 16:50:15 -0300

    conclusao materia de estrutura de selecao e operadores

--- Aula01/HelloWorld/HelloWorld/HelloWorld.cs
+++ Aula01/HelloWorld/HelloWorld/HelloWorld.cs
@@ -4,15 +4,60 @@ namespace HelloWorld
 {
     class HelloWorld
     {
+        string Idade { get; set; }
         static void Main(string[] argumentos)
         {
-            int idade = 32;
+            string nome = null;
             bool isAdulto = true;
             decimal valor = 10.55m;
             double valor2 = 10.55;
-            char sigla = 'C';
-            string estado = "Sao Paulo";
-            
+            var teste = "Teste";
+
+            int idade = 16;
+            char sigla = 'A';
+            string estado = "Bahia";
+
+            // ! negacao && =  AND e || = OR
+            if (nome != null)
+            {
+                nome.ToUpper();
+            }
+            if (isAdulto != true)
+            {
+                teste = "Menor de idade";
+            }
+            else if(estado == "Sao Paulo")
+            {
+                teste = "Paulista";
+            }
+
+            Console.WriteLine(teste);
+
+            // Switch case
+            /*
+            switch(idade)
+            {
+                case < 2: 
+                    teste = "Nenem";
+                    break;
+                case < 18: 
+                    teste = "Crianca";
+                    break;
+                case < 65: 
+                    teste = "Adulto";
+                    break;
+                default: 
+                    teste = "Melhor idade";
+                    break;
+            }
+
+            */
+
+            valor2 += 5;
+            valor2 -= 5;
+            valor2 /= 5;
+            valor2 *= 5;
+
         }
     }
 }
--- Aula01/HelloWorld/HelloWorld/Program.cs
+++ Aula01/HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         //static void Main(string[] argumentos)
         //{
-        //    Console.WriteLine("Rafael Thomazelli!");
+          //  Console.WriteLine("Rafael Thomazelli!");
         //}
     }
 }
