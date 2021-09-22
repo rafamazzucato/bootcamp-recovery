52f4faba1f72ad5c3531ce26dc99800c596d682e
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2021-09-22 17:56:42 -0300

    testes de variaveis dinamicas

---
+++ Aula12/RazorTeste/RazorTeste/Pages/Index2.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorTeste.Pages
+{
+    public class Index2Model : PageModel
+    {
+        public string Message { get; private set; }
+        public void OnGet()
+        {
+            Message = "Horario do Servidor e:" + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+        }
+    }
+}
---
+++ Aula12/RazorTeste/RazorTeste/Pages/Principal.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorTeste.Pages
+{
+    public class PrincipalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+
+    }
+}
